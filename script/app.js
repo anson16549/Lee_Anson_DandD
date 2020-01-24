63c1a7b88484caa36b1d40a6c8d50a1308b60d0d
Lee <a_lee100712@ats-hr420-dm28.fc.ca>
2020-01-24 14:40:16 -0500

    adding things

---
+++ script/app.js
@@ -0,0 +1,20 @@
+(() => {
+	// set up the puzzle pieces and boards
+	const puzzleButtons = document.querySelectorAll('#buttonHolder img');
+
+	function changeImageSet() {
+	// change all the image elements on the page -> draglbabe image sources
+	// and set the drop zone background
+	
+	debugger;
+	}
+
+
+
+//
+// add event handling here
+
+// 
+	puzzleButtons.forEach(button => button.addEventListener('click', changeImageSet))
+
+})();
