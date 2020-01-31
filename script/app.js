798f1bea7cb0eafb1baba16e0b2496d8d72cdd11
Lee <a_lee100712@ats-hr420-dm28.fc.ca>
2020-01-31 13:31:45 -0500

    updating

--- script/app.js
+++ script/app.js
@@ -1,12 +1,21 @@
 (() => {
 	// set up the puzzle pieces and boards
-	const puzzleButtons = document.querySelectorAll('#buttonHolder img');
+	const puzzleButtons = document.querySelectorAll('#buttonHolder img'),
+		puzzlePieces = document.querySelectorAll('.puzzle-pieces img'),
+		gameBoard = document.querySelector('.puzzle-board');
 
+		const pieceNames = ["topLeft", "topRight", "bottomLeft", "bottomRight"];
 	function changeImageSet() {
 	// change all the image elements on the page -> draglbabe image sources
 	// and set the drop zone background
 	
-	debugger;
+	// change the image element on the left to match the selected puzzleß
+
+	pieceNames.forEach((piece, index) => puzzlePieces[index].src = `images/${piece + this.dataset.puzzleref}.jpg`); 
+	//and set the drop zone background image base on the puzzle//
+
+	gameBoard.style.backgroundImage = `url(images/background${this.dataset.puzzleref}.jpg)`;
+	//debugger;
 	}
 
 
@@ -15,6 +24,8 @@
 // add event handling here
 
 // 
-	puzzleButtons.forEach(button => button.addEventListener('click', changeImageSet))
+	puzzleButtons.forEach(button => button.addEventListener('click', changeImageSet));
 
+// Research call, apply and bind look at MDN
+	changeImageSet.call(puzzleButtons[0]);
 })();
